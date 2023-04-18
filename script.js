fcf26b15aa720e1553c94259e5a6263b1800a779
Rémy Delanaux <remy.delanaux@liris.cnrs.fr>
2023-04-18 18:13:40 +0900

    Adaptation du code pour données élections

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 window.onload=()=>{
-    d3.csv("unemployment.csv").then(function(data) {
-        console.log(data)
-        chart = StackedAreaChart(data, {
-            x: d => d.date,
-            y: d => d.unemployed,
-            z: d => d.industry,
-            yLabel: "↑ Unemployed persons",
+
+    const cb = document.querySelector('#step');
+	cb.addEventListener('change', plotChart);
+    plotChart();
+}
+
+const PARTY_POOLS = ["Extrême gauche", "Gauche radicale", "Gauche", "Écologistes", "Centre-gauche", "Divers", "Centre", "Centre-droit", "Droite", "Droite souverainiste", "Extrême droite"];
+
+function plotChart() {
+    document.getElementById("chart").innerHTML = ''
+    d3.json("data.json", {cache: "no-store"}).then(function(data) {
+
+        const cb = document.querySelector('#step');
+        let stepFilter = cb.checked ? 2 : 1;
+        data.data.forEach((elec) => {
+            PARTY_POOLS.filter(pool => !(elec.results.some(cand => cand.pool === pool))).forEach((missingPool) => {
+                elec.results.push({
+                    "name": "[Aucun]",
+                    "res_100": 0,
+                    "res": 0,
+                    "party": "[Aucun]",
+                    "pool": missingPool,
+                    "level": -1
+                })
+            });
+        });
+        allRes = data.data.filter((elec) => elec.step == stepFilter).map(elec => {
+            elec.results.forEach(res => {
+                res["date"] = elec.date
+            });
+            return elec.results.sort((a,b) => b.level - a.level);
+        }).flat();
+        chart = StackedAreaChart(allRes, {
+            x: res => d3.timeParse("%Y-%m-%d")(res.date),
+            y: res => res.res_100,
+            z: res => res.pool,
+            yLabel: "↑ Résultat",
+            zDomain:  PARTY_POOLS.reverse(),
             width: 1200,
             height: 800
         });
         document.getElementById("chart").append(chart);
-    })    
+    })   
 }
 
 // Original baseline code:
@@ -39,16 +70,19 @@ function StackedAreaChart(data, {
     yLabel, // a label for the y-axis
     xFormat, // a format specifier string for the x-axis
     yFormat = "%", // a format specifier string for the y-axis
-    colors = d3.schemeTableau10, // an array of colors for the (z) categories
+    // colors = d3.schemeTableau10, // an array of colors for the (z) categories
   } = {}) {
     // Compute values.
     const X = d3.map(data, x);
     const Y = d3.map(data, y);
     const Z = d3.map(data, z);
-  
+    // console.log("XYZ:", X, Y, Z)
+
     // Compute default x- and z-domains, and unique the z-domain.
     if (xDomain === undefined) xDomain = d3.extent(X);
     if (zDomain === undefined) zDomain = Z;
+    
+    let origz = zDomain;
     zDomain = new d3.InternSet(zDomain);
   
     // Omit any data not present in the z-domain.
@@ -61,26 +95,44 @@ function StackedAreaChart(data, {
     // data point for a given unique x- and z-value.
     const series = d3.stack()
         .keys(zDomain)
-        .value(([x, I], z) => Y[I.get(z)])
+        .value(([x, I], z) => {
+            if (Y[I.get(z)]) {
+                return Y[I.get(z)];
+            } else {
+                return 0;
+            }
+        })
         .order(order)
         .offset(offset)
       (d3.rollup(I, ([i]) => i, i => X[i], i => Z[i]))
       .map(s => s.map(d => Object.assign(d, {i: d.data[1].get(s.key)})));
-  
+
     // Compute the default y-domain. Note: diverging stacks can be negative.
     if (yDomain === undefined) yDomain = d3.extent(series.flat(2));
   
     // Construct scales and axes.
     const xScale = xType(xDomain, xRange);
     const yScale = yType(yDomain, yRange);
-    const color = d3.scaleOrdinal(zDomain, colors);
+    const color = d3.scaleOrdinal(
+        origz.reverse(), 
+        ["#bb0000", "#dd0000", "#FF8080", "#00c000", "#ffc0c0", "#FAC577", "#ffeb00", "#00FFFF", "#0066cc", "#adc1fd", "#404040"]
+    );
+
     const xAxis = d3.axisBottom(xScale).ticks(width / 80, xFormat).tickSizeOuter(0);
     const yAxis = d3.axisLeft(yScale).ticks(height / 50, yFormat);
   
     const area = d3.area()
-        .x(({i}) => xScale(X[i]))
+        .x((point) =>  {
+            let {i} = point
+            if (i)
+                return xScale(X[i]) 
+            else {
+                let newi = X.indexOf(point['data'][0])
+                return xScale(X[newi])
+            }
+        } ) 
         .y0(([y1]) => yScale(y1))
-        .y1(([, y2]) => yScale(y2));
+        .y1(([, y2]) =>  yScale(y2))
   
     const svg = d3.create("svg")
         .attr("width", width)
